b939f3d9a5cd0c62292b2acfa4a1c804a3c6ecc0
M҉ a҉ d҉ h҉ a҉ v҉ a҉ <57581347+Madhava-mng@users.noreply.github.com>
2020-03-09 16:27:19 +0530

    Update R-5-C.sh

--- R-5-C_Repo/R-5-C.sh
+++ R-5-C_Repo/R-5-C.sh
@@ -7,22 +7,23 @@ echo "                  [ R-5-C ]               "
 echo  "      [TOOL]                  [DISCRIPT]      "
 echo  " [1] Uni-CRACKER              Hash cracker    "
 echo  " [2] Hash_Creator             Hash creater    "
-echo  " [3] ALL     "
-echo -n" [OPTION]|> "
+echo  " [3] ISU                      portScanner"
+echo  " [a] ALL     "
+echo -n " [OPTION]|> "
 read opt
 if [ $opt = '1' ]
 then
     git clone https://www.github.com/RED-5-CRACKERS/Uni-cracker.git
 elif [ $opt = '2' ]
 then
-    git clone https://www.github.com/RED-5-CRACKERS/create_hash.git
+    https://github.com/RED-5-CRACKERS/creat_hash.git
 elif [ $opt = '3' ]
 then
     git clone https://www.github.com/RED-5-CRACKERS/ISU.git
 elif [ $opt = 'a' ]
 then
     git clone https://www.github.com/RED-5-CRACKERS/Uni-cracker.git
-    git clone https://www.github.com/RED-5-CRACKERS/create_hash.git
+    git clone https://www.github.com/RED-5-CRACKERS/creat_hash.git
     git clone https://www.github.com/RED-5-CRACKERS/ISU.git
 fi
 
