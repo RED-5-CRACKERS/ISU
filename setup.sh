9a24ad9ae7e225416f3acc3b06f1bf089fa2d6f1
M҉ a҉ d҉ h҉ a҉ v҉ a҉ <57581347+Madhava-mng@users.noreply.github.com>
2020-03-09 15:57:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,16 +9,13 @@ echo simply type isu
 
 echo -n "[ YOU WANT TO DOWNLOAD 'UNI-CRACKER' ] > "
 read ins
-while true
-do
-  if [ $ins = 'y' ]
-  then
-    git clone https://www.github.com/RED-5-CRACKERS/Uni-cracker.git
-    echo [ THANKS ! ]
-  elif [ $ins = 'n' ]
-  then
-    echo [ BYE ... ]
-    break
-  fi
-done
+
+if [ $ins = 'y' ]
+then
+  git clone https://www.github.com/RED-5-CRACKERS/Uni-cracker.git
+  echo [ THANKS ! ]
+elif [ $ins = 'n' ]
+then
+  echo [ BYE ... ]
+fi
 isu -h
