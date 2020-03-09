f83c92ada423227104b307deea7b09b4362c138e
M҉ a҉ d҉ h҉ a҉ v҉ a҉ <57581347+Madhava-mng@users.noreply.github.com>
2020-03-09 15:42:23 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,18 @@ chmod -w isu.py
 chmod +x isu.py
 echo simply type isu
 
-echo [*]  isu -h
+echo -n "[ YOU WANT TO DOWNLOAD 'UNI-CRACKER' ] > "
+read ins
+while true
+do
+  if [ $ins = 'y' ]
+  then
+    git clone https://www.github.com/RED-5-CRACKERS/Uni-cracker.git
+    echo [ THANKS ! ]
+  elif [ $ins = 'n' ]
+  then
+    echo [ BYE ... ]
+    break
+  fi
+done
+isu -h
