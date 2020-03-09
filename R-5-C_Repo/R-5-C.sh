55a34cac4cf621da60b10ec7847e1e62d7fe8df3
M҉ a҉ d҉ h҉ a҉ v҉ a҉ <57581347+Madhava-mng@users.noreply.github.com>
2020-03-09 16:32:37 +0530

    Update R-5-C.sh

--- R-5-C_Repo/R-5-C.sh
+++ R-5-C_Repo/R-5-C.sh
@@ -16,7 +16,7 @@ then
     git clone https://www.github.com/RED-5-CRACKERS/Uni-cracker.git
 elif [ $opt = '2' ]
 then
-    https://github.com/RED-5-CRACKERS/creat_hash.git
+    https://www.github.com/RED-5-CRACKERS/creat_hash.git
 elif [ $opt = '3' ]
 then
     git clone https://www.github.com/RED-5-CRACKERS/ISU.git
