f5e6d9b41e7d60a97b4a5d9b571b9eb297d7ed8e
M҉ a҉ d҉ h҉ a҉ v҉ a҉ <57581347+Madhava-mng@users.noreply.github.com>
2020-03-09 09:42:08 +0530

    Update setup-for-termux.sh

--- setup-for-termux.sh
+++ setup-for-termux.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-cp isu.py 
+cp isu.py /data/data/com.termux/files/usr/bin/isu
+chmod +x /data/data/com.termux/files/usr/bin/isu
+chmod +x isu.py
+rm -v setup-for-termux.sh setup.sh
+echo [*] simply isu
+isu -h
