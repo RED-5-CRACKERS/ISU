41e8ddc080dbf99a037e6735177e4211e63d0364
M҉ a҉ d҉ h҉ a҉ v҉ a҉ <57581347+Madhava-mng@users.noreply.github.com>
2020-03-09 15:35:02 +0530

    Update setup-for-termux.sh

--- setup-for-termux.sh
+++ setup-for-termux.sh
@@ -4,19 +4,22 @@ cp isu.py /data/data/com.termux/files/usr/bin/isu
 chmod +x /data/data/com.termux/files/usr/bin/isu
 chmod +x isu.py
 rm -v setup-for-termux.sh setup.sh
-echo [*] simply isu
-isu -h
+echo "[*] simply isu"
 echo
 echo
 echo
 echo -n "[ IF YOU WANT TO INSTALL MY ANOTHER SCRIPTS [Y|N]] > "
 read ins
-
-if [ $ins = 'y' ]
-then
-    echo [THANKS]
-    git clone https://github.com/RED-5-CRACKERS/Uni-cracker.git
-else
-    echo [ BYE.... ]
+while true
+    if [ $ins = 'y' ]
+    then
+        echo [THANKS]
+        git clone https://github.com/RED-5-CRACKERS/Uni-cracker.git
+    elif [ $ins = 'n']
+    then
+        echo [ BYE.... ]
+        break
 
 fi
+isu -h
+
